c8d95b0693f04eddcab2bbadce878b728c99f8f5
ShreddShark <daniel.cox2685@gmail.com>
2023-06-20 16:15:50 -0500

    completed sitewide html/css changes, added contact for, footer

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,11 @@
-import Splide from '@splidejs/splide';
-new Splide( '.splide' ).mount();
+const splide = new Splide( '.splide' );
+const bar    = splide.root.querySelector( '.my-carousel-progress-bar' );
 
-mount()
+// Updates the bar width whenever the carousel moves:
+splide.on( 'mounted move', function () {
+  let end  = splide.Components.Controller.getEnd() + 1;
+  let rate = Math.min( ( splide.index + 1 ) / end, 1 );
+  bar.style.width = String( 100 * rate ) + '%';
+} );
+
+splide.mount();
